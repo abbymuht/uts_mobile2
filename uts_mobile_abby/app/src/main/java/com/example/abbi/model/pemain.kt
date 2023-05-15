9d5cc36add16bc712b651921f78f4a117dafc336
Abby Muhammad Titan <abbymt2312@gmail.com>
2023-05-15 22:09:25 +0700

    menambahkan file uts_mobile_abby

---
+++ uts_mobile_abby/app/src/main/java/com/example/abbi/model/pemain.kt
@@ -0,0 +1,10 @@
+package com.example.abbi.model
+
+data class pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi :String,
+    var tinggi : String,
+    var Tempatlahir : String,
+    var tgllahir : String
+)
